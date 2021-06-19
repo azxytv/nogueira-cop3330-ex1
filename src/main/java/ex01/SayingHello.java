bb8dcbebf1e09eb1c7fcbb3faf93519e74b4b09b
Adam <azxygaming@gmail.com>
2021-06-19 13:16:44 -0400

    initial solution problem 1

--- src/main/java/ex01/SayingHello.java
+++ src/main/java/ex01/SayingHello.java
@@ -2,6 +2,15 @@ package ex01;
 import java.util.Scanner;
 
 public class SayingHello {
+    public static void main(String[] args) {
 
 
+    Scanner in = new Scanner(System.in);
+    System.out.print("What is your name?");
+    String name = in.nextLine();
+
+    String out = "Hello, " + name + " , nice to meet you!";
+
+    System.out.println(out);
+}
 }
